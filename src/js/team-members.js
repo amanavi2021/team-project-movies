7f3851acdbed475a1ac88ebb3cadafb2bfab6efb
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-04-07 23:22:54 +0300

    Завершує футер

---
+++ src/js/team-members.js
@@ -0,0 +1,97 @@
+export const teamMembers = [
+  {
+    name: "",
+    role: "Team Lead",
+    github: "",
+    linkedin: "",
+    photo: "",
+  },
+  {
+    name: "Oleksandr Karpiuk",
+    role: "Scrum Master",
+    github: "https://github.com/Oleksandr-Karpiuk",
+    linkedin: "https://www.linkedin.com/in/oleksandr-karpiuk/",
+    photo:
+      "https://media.licdn.com/dms/image/D4E03AQHaiyyHccyRGg/profile-displayphoto-shrink_200_200/0/1680380173309?e=1686182400&v=beta&t=N-mOTz8Drif8wND3Ba9EZb9o3lsC8ZuSTgY3uNj0hEY",
+  },
+  {
+    name: "Olga Tolstykhina",
+    role: "Developer",
+    github: "https://github.com/OlgaT0709",
+    linkedin: "https://www.linkedin.com/in/olga-tolstikhina-developer/",
+    photo:
+      "https://media.licdn.com/dms/image/D4E03AQEc2RMSdFKOrQ/profile-displayphoto-shrink_800_800/0/1676054506466?e=1686182400&v=beta&t=tzw488uBPpHbiGaOKgRrV7J62Ra_CU_VIwZisZ3CPWk",
+  },
+  {
+    name: "Liliia Paliichuk",
+    role: "Developer",
+    github: "",
+    linkedin: "",
+    photo: "",
+  },
+  {
+    name: "Natalia Malovana",
+    role: "Developer",
+    github: "",
+    linkedin: "",
+    photo: "",
+  },
+  {
+    name: "Ulyana Yashan",
+    role: "Developer",
+    github: "https://github.com/UlyanaYashan",
+    linkedin: "",
+    photo: "",
+  },
+  {
+    name: "Ruslan Bulda",
+    role: "Developer",
+    github: "https://github.com/rbulda50",
+    linkedin:
+      "https://www.linkedin.com/in/%D1%80%D1%83%D1%81%D0%BB%D0%B0%D0%BD-%D0%B1%D1%83%D0%BB%D0%B4%D0%B0-8a0345204/",
+    photo:
+      "https://media.licdn.com/dms/image/C4D03AQE9yuFbOvfwpA/profile-displayphoto-shrink_200_200/0/1610811042896?e=1686182400&v=beta&t=rLrvi32cplyT9SpnZcs-BwokiyGAhtGerj1d7gsuUHw",
+  },
+  {
+    name: "",
+    role: "Developer",
+    github: "",
+    linkedin: "",
+    photo: "",
+  },
+  {
+    name: "",
+    role: "Developer",
+    github: "",
+    linkedin: "",
+    photo: "",
+  },
+  {
+    name: "",
+    role: "Developer",
+    github: "",
+    linkedin: "",
+    photo: "",
+  },
+  {
+    name: "",
+    role: "Developer",
+    github: "",
+    linkedin: "",
+    photo: "",
+  },
+  {
+    name: "",
+    role: "Developer",
+    github: "",
+    linkedin: "",
+    photo: "",
+  },
+  {
+    name: "",
+    role: "Developer",
+    github: "",
+    linkedin: "",
+    photo: "",
+  },
+];
