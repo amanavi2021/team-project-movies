2b48ecdcdf7d21c24a918813c37d13a95213ace4
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-04-08 12:36:34 +0300

    Додає модальне вікно

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import apiService from './js/apiService';
+import { toggleModal } from './js/modal';
 
+toggleModal();
 saveGenres();
 
-
 async function saveGenres() {
-    
-    if (!localStorage.getItem("genres")) {
-        const fetchedGenres = await apiService.fetchGenres();
-        const genres = fetchedGenres.genres;
-        localStorage.setItem("genres", JSON.stringify(genres));
-    }
+  if (!localStorage.getItem('genres')) {
+    const fetchedGenres = await apiService.fetchGenres();
+    const genres = fetchedGenres.genres;
+    localStorage.setItem('genres', JSON.stringify(genres));
+  }
 }
-
-
-
-
