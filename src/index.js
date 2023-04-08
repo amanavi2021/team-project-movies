950b4f1afb75b887fbceef1181293ede7ff64fc3
OlesiaManina <113991075+OlesiaManina@users.noreply.github.com>
2023-04-08 12:43:51 +0300

    loader
    наче працює. по візуалу або функціоналу можна вносити пропозиції. зробила як зрозуміла

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import apiService from './js/apiService';
+import {mask} from './js/loader';
 
 saveGenres();
 
--- src/my-library.js
+++ src/my-library.js
@@ -1,2 +1,3 @@
 
 import apiService from './js/apiService';
+import {mask} from './js/loader';
