19af7244737256de851af256e916d14b18e6aeb2
Olga Tolstykhina <ovt0709@gmail.com>
2023-04-08 21:46:06 +0300

    gallery style

--- src/js/renderFilms.js
+++ src/js/renderFilms.js
@@ -3,19 +3,23 @@ import apiService from './apiService'
 
 
 export default async function renderFilms(films) {
-    const genresResponse = await apiService.fetchGenres();
-    const genres = genresResponse.genres;
-
-   console.log(films);
+  const genresResponse = await apiService.fetchGenres();
+  const genres = genresResponse.genres;
 
   return films.map(({ poster_path, title, genre_ids, release_date }) => {
     const date = new Date(release_date);
     const year = date.getFullYear();
        
-      const genreList = genre_ids.map((genreId) => {
-          const genre = genres.find((g) => g.id === genreId);
-          return genre.name;
-      });
+    let genreList = genre_ids.map((genreId) => {
+        const genre = genres.find((g) => g.id === genreId);
+        return genre.name;
+    });
+
+    if (genreList.length > 2) {
+      genreList = genreList.slice(0, 2);
+      genreList.push('Other');
+    }
+    genreList = genreList.join(', ');
 
     return markupTpl({ poster_path, title, genreList, year });
   }).join('');
