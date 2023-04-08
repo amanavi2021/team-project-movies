27b8c30105ecf75aa9093159b795f131e4d97308
Nadiia Maltseva <amanavi2021@gmail.com>
2023-04-08 19:17:16 +0300

    завантажую постери

--- src/js/renderFilms.js
+++ src/js/renderFilms.js
@@ -5,8 +5,10 @@ import apiService from './apiService'
 export default async function renderFilms(films) {
     const genresResponse = await apiService.fetchGenres();
     const genres = genresResponse.genres;
-    
-  return films.map(({ backdrop_path, title, genre_ids, release_date }) => {
+
+   console.log(films);
+
+  return films.map(({ poster_path, title, genre_ids, release_date }) => {
     const date = new Date(release_date);
     const year = date.getFullYear();
        
@@ -15,6 +17,6 @@ export default async function renderFilms(films) {
           return genre.name;
       });
 
-    return markupTpl({ backdrop_path, title, genreList, year });
+    return markupTpl({ poster_path, title, genreList, year });
   }).join('');
 };
