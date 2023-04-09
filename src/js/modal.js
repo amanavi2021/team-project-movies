33033fb00cd0f00b16d0f3d14d04a14652a5bad5
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-04-10 01:21:05 +0300

    conflicts fixed dif padings

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,9 +1,15 @@
+import markupModalMovie from '../templates/markupModalMovie.hbs';
+import apiService from './apiService';
+
 export function toggleModal() {
   const refs = {
     openModal: document.querySelector('[data-modal-open]'),
     closeModal: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    filmClick: document.querySelector('.gallery'),
+    filmInfo: document.querySelector('.film-info__wrap'),
   };
+  
 
   function openModal() {
     refs.modal.classList.remove('is-hidden');
@@ -13,8 +19,8 @@ export function toggleModal() {
 
   function closeModal() {
     refs.modal.classList.add('is-hidden');
-    window.removeEventListener('keydown', closeModalOnEsc); 
-    refs.modal.removeEventListener('click', closeModalOnClickOutside); 
+    window.removeEventListener('keydown', closeModalOnEsc);
+    refs.modal.removeEventListener('click', closeModalOnClickOutside);
   }
 
   function closeModalOnEsc(event) {
@@ -31,7 +37,26 @@ export function toggleModal() {
 
   refs.openModal.addEventListener('click', openModal);
   refs.closeModal.addEventListener('click', closeModal);
-}
+  refs.filmClick.addEventListener('click', onClickOpen);
+  refs.closeModal.addEventListener('click', onClickClose);
 
+  function onClickOpen() {
+    refs.modal.classList.remove('is-hidden');
+    renderList();
+  }
+
+  function onClickClose() {
+    refs.modal.classList.add('is-hidden');
+    clearModalMovie(refs.filmInfo);
+  }
+
+  function renderList() {
+    refs.filmInfo.insertAdjacentHTML(`beforeend`, markupModalMovie());
+  }
+
+  function clearModalMovie(ref) {
+    ref.innerHTML = '';
+  }
+}
 
 toggleModal();
