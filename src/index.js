a674577828d350accf665957ed079e054d3a23cd
Liliia Paliichuk <113315152+lili2628@users.noreply.github.com>
2023-04-09 03:32:10 +0200

    added black-version of pagination

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import apiService from './js/apiService';
 import onLoad from './js/onLoad' 
 import {mask} from './js/loader';
 import { toggleModal } from './js/modal';
+import { paginationFeach } from './js/pagination';
 
 toggleModal();
 saveGenres();
-onLoad();
+paginationFeach();
 
 async function saveGenres() {
   if (!localStorage.getItem('genres')) {
