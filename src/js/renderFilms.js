673d4aa1f29818b90f325c1303fb653672ec7104
Stiroid <stakhovsky89@gmail.com>
2023-04-09 13:18:02 +0300

    film-card update

---
+++ src/js/renderFilms.js
@@ -0,0 +1,20 @@
+import markupTpl from '../templates/markupFilmMainPage.hbs';
+import apiService from './apiService'
+
+
+export default async function renderFilms(films) {
+    const genresResponse = await apiService.fetchGenres();
+    const genres = genresResponse.genres;
+    
+  return films.map(({ poster_path, title, genre_ids, release_date }) => {
+    const date = new Date(release_date);
+    const year = date.getFullYear();
+       
+      const genreList = genre_ids.map((genreId) => {
+          const genre = genres.find((g) => g.id === genreId);
+          return genre.name;
+      });
+
+    return markupTpl({ poster_path, title, genreList, year });
+  }).join('');
+};
