b65b2cc9e7a7baee2f8119e387354fb55e0608f3
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-04-10 00:25:03 +0300

    modal done

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,45 +1,39 @@
 import markupModalMovie from '../templates/markupModalMovie.hbs';
 import apiService from './apiService';
 
-
 export function toggleModal() {
   const refs = {
     openModal: document.querySelector('[data-modal-open]'),
     closeModal: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-    testbtn: document.querySelector('.button-movie'),
+    filmClick: document.querySelector('.gallery'),
     filmInfo: document.querySelector('.film-info__wrap'),
-    };
+  };
 
-    function renderList() {
-       refs.filmInfo.insertAdjacentHTML(`beforeend`, markupModalMovie());
-    }
+  function renderList() {
+    refs.filmInfo.insertAdjacentHTML(`beforeend`, markupModalMovie());
+  }
 
   refs.openModal.addEventListener('click', toggleModal);
   refs.closeModal.addEventListener('click', toggleModal);
-    refs.testbtn.addEventListener('click', onClickOpen);
-    refs.closeModal.addEventListener('click', onClickClose);
-    
+  refs.filmClick.addEventListener('click', onClickOpen);
+  refs.closeModal.addEventListener('click', onClickClose);
 
-    function onClickOpen() {
-        refs.modal.classList.remove('is-hidden'); 
-        renderList();
-    }
+  function onClickOpen() {
+    refs.modal.classList.remove('is-hidden');
+    renderList();
+  }
 
-    function onClickClose() {
-        refs.modal.classList.add('is-hidden'); 
-        clearModalMovie(refs.filmInfo);
-    }
-
+  function onClickClose() {
+    refs.modal.classList.add('is-hidden');
+    clearModalMovie(refs.filmInfo);
+  }
 
   function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-    }
+    refs.modal.classList.toggle('is-hidden');
+  }
 
-    function clearModalMovie(ref) {
-         ref.innerHTML = '';
-    }
+  function clearModalMovie(ref) {
+    ref.innerHTML = '';
+  }
 }
-
-
-
