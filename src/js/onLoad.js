03d4c3e30cbe0e16ccd4b59719f6084abd4ef2ba
Olga Tolstykhina <ovt0709@gmail.com>
2023-04-10 05:45:39 +0300

    youtube
    черновой вариант

--- src/js/onLoad.js
+++ src/js/onLoad.js
@@ -3,11 +3,12 @@ import renderFilms from './renderFilms';
 import localStore from './service/localstorage'
 import notifier from './service/notifier'
 import refs from './service/refs';
+import onClickPlayer from './trailerplayer';
 
 export default async function onLoad() {
-   
-    localStore.remove('trandingFilmDay') ;
-    
+
+    localStore.remove('trandingFilmDay');
+
     try {
 
         await apiService.saveTrandingFilmDayToLocalStorage();
@@ -15,9 +16,14 @@ export default async function onLoad() {
         console.log('films onload', films);
         refs.filmsContainer.insertAdjacentHTML('beforeend', await renderFilms(films.results));
     } catch { catchError };
+
+    //  Вішаємо слухача на кнопку на картці і при натиску, запускаємо Відео
+    const trailerPlayBTNs = document.querySelectorAll('.trailer-player-btn');
+    trailerPlayBTNs.forEach((button) => button.addEventListener('click', onClickPlayer));
 };
 
 function catchError(error) {
     notifier.error('Something went wrong...');
     console.error('Get onLoad error: ', error.message);
+
 };
