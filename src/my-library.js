21e8efd5be060a2dac35d8f74cdc8ff5b8058556
rbulda50 <117113002+rbulda50@users.noreply.github.com>
2023-04-10 22:48:45 +0300

    bugfixed
    fixed bugs; added styles for active btn in modal-movie

--- src/my-library.js
+++ src/my-library.js
@@ -7,40 +7,66 @@ import * as teamModal from './js/team-members';
 import localstorage from './js/service/localstorage';
 import renderFilms from './js/renderFilms';
 
-onLoadCurrentFilms()
+onLoadCurrentFilms();
 const filmContainer = document.querySelector('.my-gallery');
 
 const buttons = document.querySelector('.header__buttons-library');
 buttons.addEventListener('click', onClickBtnLibrary);
 
-
 const btnQueue = document.querySelector('[data-add="queue"]');
 const btnWatched = document.querySelector('[data-add="watched"]');
 
 function onClickBtnLibrary(e) {
+    const target = e.target;
+    if (target === btnQueue) {
+        if (!localstorage.load('queue')) {
+            onClassActiveToggle(target);
+            return;
+        } else {
+            const parsedFilmsFromQueueLocalStorage = localstorage.load('queue');
+            onClassActiveToggle(target);
 
-    if (e.target === btnQueue) {
-        const parsedFilmsFromQueueLocalStorage = localstorage.load('queue');
-        btnWatched.classList.remove('btn--active');
-        btnQueue.classList.add('btn--active');
-        return appendFromLocalStorage(parsedFilmsFromQueueLocalStorage);
-    }
-    if (e.target === btnWatched) {
-        const parsedFilmsFromWatchedLocalStorage = localstorage.load('watched');
-        btnWatched.classList.add('btn--active');
-        btnQueue.classList.remove('btn--active');
-        return appendFromLocalStorage(parsedFilmsFromWatchedLocalStorage);
+            appendFromLocalStorage(parsedFilmsFromQueueLocalStorage);
+            return;
+        };
+    };
+
+    if (target === btnWatched) {
+        if (!localstorage.load('watched')) {
+            onClassActiveToggle(target);
+            return;
+        } else {
+            const parsedFilmsFromWatchedLocalStorage = localstorage.load('watched');
+            appendFromLocalStorage(parsedFilmsFromWatchedLocalStorage);
+            onClassActiveToggle(target);
+            return;
+        };
     };
 };
 
 async function appendFromLocalStorage(parsedFilms) {
-    const markup = await renderFilms(parsedFilms).then(result => { return result });
+    try {
+        const markup = await renderFilms(parsedFilms).then(result => result);
         filmContainer.innerHTML = markup;
+    } catch (error) {
+        console.error(error);
+    };
 };
 
-async function onLoadCurrentFilms() {
-    return await appendFromLocalStorage(localstorage.load('watched'))
+function onLoadCurrentFilms() {
+    if (!localstorage.load('watched')) {
+        return;
+    }
+    appendFromLocalStorage(localstorage.load('watched'));
 }
 
-
+function onClassActiveToggle(target) {
+    if (target === btnQueue) {
+        btnWatched.classList.remove('btn--active');
+        btnQueue.classList.add('btn--active');
+    } else {
+        btnWatched.classList.add('btn--active');
+        btnQueue.classList.remove('btn--active');
+    };
+};
 
