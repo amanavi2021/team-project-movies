906b50b5356fdd431fd2500f7ee25e6a769faf46
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-04-10 19:30:19 +0300

    Додає закриття на esc і backdrop

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,7 +12,6 @@ export function toggleModal() {
     filmInfo: document.querySelector('.film-info__wrap'),
   };
 
-
   // function openModal(e) {
   //   if (
   //     e.target === e.currentTarget ||
@@ -28,18 +27,21 @@ export function toggleModal() {
   //   renderList();
   // }
 
+  window.removeEventListener('keydown', closeModalOnEsc);
+  refs.modal.removeEventListener('click', closeModalOnClickOutside);
 
   function closeModal() {
     refs.modal.classList.add('is-hidden');
-    window.removeEventListener('keydown', closeModalOnEsc);
-    refs.modal.removeEventListener('click', closeModalOnClickOutside);
+    document.body.style.overflow = '';
     clearModalMovie(refs.filmInfo);
   }
 
   function closeModalOnEsc(event) {
-    if (event.code === 'Escape') {
-      closeModal();
+    if (event.code !== 'Escape') {
+      return;
     }
+    window.removeEventListener('keydown', closeModalOnEsc);
+    closeModal();
   }
 
   function closeModalOnClickOutside(event) {
@@ -53,35 +55,32 @@ export function toggleModal() {
   refs.filmClick.addEventListener('click', onClickOpen);
   refs.closeModal.addEventListener('click', closeModal);
 
-
   async function onClickOpen(e) {
-       if (
-         e.target === e.currentTarget ||
-         e.target.nodeName === `BUTTON` ||
-         e.target.classList.contains(`trailer-player-wrapper`)
-       ) {
-         return;
-       }
+    if (
+      e.target === e.currentTarget ||
+      e.target.nodeName === `BUTTON` ||
+      e.target.classList.contains(`trailer-player-wrapper`)
+    ) {
+      return;
+    }
     console.log('id', e.target.dataset.id);
 
     try {
+      let movieId = e.target.dataset.id;
+      const movies = apiService.getSavedFilms();
 
-       let movieId = e.target.dataset.id;
-       const movies = apiService.getSavedFilms();
+      console.log('movies', movies);
 
-       console.log('movies', movies);
-
-       const movie = movies.results.find(({id}) => id === Number(movieId));
-       console.log('movie by method find', movie)
-       //  refs.filmInfo.insertAdjacentHTML('beforeend', renderList(movie)) ;
-       refs.filmInfo.innerHTML = await renderList(movie);
-
-       
-    } 
-    catch (error) {
+      const movie = movies.results.find(({ id }) => id === Number(movieId));
+      console.log('movie by method find', movie);
+      //  refs.filmInfo.insertAdjacentHTML('beforeend', renderList(movie)) ;
+      refs.filmInfo.innerHTML = await renderList(movie);
+    } catch (error) {
       console.error(error);
     }
+    window.addEventListener('keydown', closeModalOnEsc);
     refs.modal.classList.remove('is-hidden');
+    document.body.style.overflow = 'hidden';
     renderList();
   }
 
@@ -90,32 +89,48 @@ export function toggleModal() {
   //   clearModalMovie(refs.filmInfo);
   // }
 
- async function renderList(movie) {
+  async function renderList(movie) {
     // console.log('renderList called with movie:', movie);
     await apiService.saveGenresToLocalStorage();
-    const genres = localStore.load('genres') || [] ;
+    const genres = localStore.load('genres') || [];
     // console.log('GENRES', genres);
-        const { poster_path, title, genre_ids, release_date, id, popularity, vote_average, vote_count, overview
+    const {
+      poster_path,
+      title,
+      genre_ids,
+      release_date,
+      id,
+      popularity,
+      vote_average,
+      vote_count,
+      overview,
     } = movie;
 
-   if (poster_path !== null) {
-        const date = new Date(release_date);
-        const year = date.getFullYear();
-        let genreList = genre_ids.map((genreId) => {
-            const genre = genres.find((g) => g.id === genreId);
-            return genre.name;
-          });
+    if (poster_path !== null) {
+      const date = new Date(release_date);
+      const year = date.getFullYear();
+      let genreList = genre_ids.map(genreId => {
+        const genre = genres.find(g => g.id === genreId);
+        return genre.name;
+      });
       if (genreList.length > 2 || genre_ids.length === 0) {
         genreList = genreList.slice(0, 2);
         genreList.push('Other');
-      };
+      }
       genreList = genreList.join(', ');
-      return markupModalMovie({ poster_path, title, genreList, year, id, popularity, vote_average, vote_count, overview
+      return markupModalMovie({
+        poster_path,
+        title,
+        genreList,
+        year,
+        id,
+        popularity,
+        vote_average,
+        vote_count,
+        overview,
       });
-   }
-      
     }
-  
+  }
 
   function clearModalMovie(ref) {
     ref.innerHTML = '';
@@ -123,4 +138,3 @@ export function toggleModal() {
 }
 
 toggleModal();
- 
