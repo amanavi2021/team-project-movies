92250338d10e9b47099dd14e84a720dff847dcd2
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-04-11 13:50:59 +0300

    fix modal in library

--- src/my-library.js
+++ src/my-library.js
@@ -2,7 +2,7 @@
 import apiService from './js/apiService';
 import {mask} from './js/loader';
 import * as teamModal from './js/team-members';
-// import toggleModal from './js/modal';
+import toggleModal from './js/modal';
 //import { paginationLocalStorage } from './js/pagination-library';
 import localstorage from './js/service/localstorage';
 import renderFilms from './js/renderFilms';
