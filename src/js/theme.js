2fa52df967b5224bf579909c835c5014ec039c0a
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-04-12 16:55:57 +0300

    change icon for theme

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,6 +1,7 @@
 export default document
-  .querySelector('.themetoggle')
-  .addEventListener('click', event => {
+  .querySelector('#switch')
+  .addEventListener('change', event => {
+    console.log('sdsdsdsd');
     event.preventDefault();
     if (localStorage.getItem('theme') === 'dark') {
       localStorage.removeItem('theme');
@@ -13,20 +14,17 @@ export default document
 function addDarkClassToHTML() {
   try {
     if (localStorage.getItem('theme') === 'dark') {
+      document.querySelector('#switch').setAttribute('checked', 'checked');
       document
         .querySelector('.footer__container')
         .classList.add('dark__footer');
-      document.querySelector('.themetoggle span').textContent = 'dark_mode';
       document.querySelector('body').classList.add('dark');
       document.querySelector('.modal').classList.add('dark');
-      document
-        .querySelector('.member__link')
-        .classList.add('dark_theme-text');
+      document.querySelector('.member__link').classList.add('dark_theme-text');
     } else {
       document
         .querySelector('.footer__container')
         .classList.remove('dark__footer');
-      document.querySelector('.themetoggle span').textContent = 'wb_sunny';
       document.querySelector('body').classList.remove('dark');
       document.querySelector('.modal').classList.remove('dark');
 
