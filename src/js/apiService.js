26430a3509d0da30d03ad264b8e17fcb683fdf5d
amanavi <105625270+amanavi2021@users.noreply.github.com>
2023-04-13 15:22:34 +0300

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import localStore from './service/localstorage';
+import localStore from './service/localstorage'
 
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '837953248391225ae7c8e73f09921895';
@@ -8,51 +8,42 @@ const LOCAL_STORAGE_G = 'genres';
 const LOCAL_STORAGE_Q = 'queue';
 const LOCAL_STORAGE_W = 'watched';
 
-let language = 'en-US';
-function checkedLanguage() {
-  if (localStorage.getItem('language') === 'ua') {
-    language = 'uk-UA';
-  } else {
-    language = 'en-US';
-  }
-}
-
 //клас робить HTTP-запит на ресурс і повертає дані (об'єкт)
 class ApiService {
-  constructor() {
-    this.searchQuery = ''; // Chupa для перевірки;
-    this.pageNumber = 1;
-    this.filmID = ''; // 594767 для перевірки;
-  }
+    constructor() {
+        this.searchQuery = '' ; // Chupa для перевірки;
+        this.pageNumber = 1;
+        this.filmID = ''; // 594767 для перевірки;
+     
+           }
 
- // запит  найпопулярніших фільмів дня       
+// запит  найпопулярніших фільмів дня       
     async fetchTrandingFilmDay() {    
-    checkedLanguage()
-        const queryString = `${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=${this.pageNumber}&language=${language}`;
+        const queryString = `${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=${this.pageNumber}`;
         return this.#fetchData(queryString); 
     }
 
    
 // запит фільма за ключовим словом 
     async fetchFilmByName() {    
-checkedLanguage()
+
         //const queryString = `${BASE_URL}search/keyword?api_key=${API_KEY}&query=${this.searchQuery}`;
-        const queryString = `${BASE_URL}/search/movie?api_key=${API_KEY}&page=${this.pageNumber}&query=${this.searchQuery}&language=${language}`;
+        const queryString = `${BASE_URL}/search/movie?api_key=${API_KEY}&page=${this.pageNumber}&query=${this.searchQuery}`;
         return this.#fetchData(queryString); 
        
     }
     
 // запит повної інформації за ID
     async fetchFullInfoByID() {    
-checkedLanguage()
-        const queryString = `${BASE_URL}movie/${this.filmID}?api_key=${API_KEY}&language=${language}`;
+
+        const queryString = `${BASE_URL}movie/${this.filmID}?api_key=${API_KEY}`;
         return this.#fetchData(queryString); 
     }
 
 // запит трейлера за ID  
      async fetchTrailerByID() {    
-checkedLanguage()
-        const queryString = `${BASE_URL}movie/${this.filmID}/videos?api_key=${API_KEY}&language=${language}`;
+
+        const queryString = `${BASE_URL}movie/${this.filmID}/videos?api_key=${API_KEY}`;
         return this.#fetchData(queryString); 
     }
 
@@ -97,8 +88,7 @@ checkedLanguage()
 
 // запит переліку жанрів 
     async #fetchGenres(){
-    checkedLanguage()
-        const queryString =`${BASE_URL}genre/movie/list?api_key=${API_KEY}&language=${language}`;
+        const queryString =`${BASE_URL}genre/movie/list?api_key=${API_KEY}`;
         return this.#fetchData(queryString); 
     }
     
@@ -142,3 +132,4 @@ checkedLanguage()
 }
 
 export default new ApiService();
+
