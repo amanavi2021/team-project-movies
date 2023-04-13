079c8be1b2e3efc400f6b787f0ee9ac8df43bc42
OlesiaManina <113991075+OlesiaManina@users.noreply.github.com>
2023-04-14 00:21:14 +0300

    оптимізованні зображення
    поки я їх не використала, бо щось не бачу через css. можливо після пулл реквесту вони будуть відображатися

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,7 +1,7 @@
+
 const mask = document.querySelector('.mask');
 
     window.addEventListener('load', () => {
-        console.log("done");
         mask.classList.add('visually-hidden');
         setTimeout(() => {
             mask.remove(); 
