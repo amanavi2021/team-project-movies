4ed16113fa825d83db79a10ec8a1ef3379027142
amanavi <105625270+amanavi2021@users.noreply.github.com>
2023-04-13 15:17:54 +0300

    Update add-to-queue-or-watched.js

--- src/js/add-to-queue-or-watched.js
+++ src/js/add-to-queue-or-watched.js
@@ -1,130 +1,116 @@
-import localstorage from './service/localstorage';
+import localstorage from "./service/localstorage";
 
 const modalContainer = document.querySelector('.modal');
-const filmContainer = document.querySelector('.modal');
+
 modalContainer.addEventListener('click', onClickBtn);
 
 let queueBtn = null;
 let watchedBtn = null;
 
-filmContainer.addEventListener('click', onClickBtn);
-
 const queue = localstorage.load('queue') || [];
 const watched = localstorage.load('watched') || [];
 
 function onClickBtn(e) {
-  const target = e.target;
-  const id = target.closest('div').dataset.id;
+    const target = e.target;
+    const id = target.closest('div').dataset.id;
 
-  if (target.id === 'queueInModal') {
-    getBtnRefs(target);
-    return addToQueue(id);
-  }
-  if (target.id === 'watchedInModal') {
-    getBtnRefs(target);
-    return addToWatched(id);
-  }
-}
+    if (target.id === 'queueInModal') {
+        getBtnRefs(target)
+        return addToQueue(id);
+    };
+    if (target.id === 'watchedInModal') {
+        getBtnRefs(target)
+        return addToWatched(id);
+    };
+};
 
 export function addToQueue(id) {
-  // console.log(id);
-  const filmsFromLocalStorage = localstorage.load('currentFilms');
-  const indexFilm = queue.findIndex(film => film.id === Number(id));
-  const findFilm = queue.find(film => film.id === Number(id));
+    // console.log(id);
+    const filmsFromLocalStorage = localstorage.load('currentFilms');
+    const indexFilm = queue.findIndex(film => film.id === Number(id));
+    const findFilm = queue.find(film => film.id === Number(id));
 
-  for (const film of filmsFromLocalStorage.results) {
-    if (film.id === Number(id)) {
-      results = {
-        genre_ids: film.genre_ids,
-        release_date: film.release_date,
-        poster_path: film.poster_path,
-        backdrop_path: film.backdrop_path,
-        title: film.title,
-        popularity: film.popularity,
-        vote_average: film.vote_average,
-        vote_count: film.vote_count,
-        overview: film.overview,
-        id: film.id,
-      };
-    }
-  }
+    let results = {};
 
-  if (findFilm) {
-    queue.splice(indexFilm, 1);
-    localstorage.save('queue', queue);
-    queueBtn.classList.remove('button-list--active');
-    if (localStorage.getItem('language') === 'ua') {
-      queueBtn.textContent = 'Додати до черги';
+    for (const film of filmsFromLocalStorage.results) {
+        if (film.id === Number(id)) {
+            results = {
+                genre_ids: film.genre_ids,
+                release_date: film.release_date,
+                poster_path: film.poster_path,
+                backdrop_path: film.backdrop_path,
+                title: film.title,
+                popularity: film.popularity,
+                vote_average: film.vote_average,
+                vote_count: film.vote_count,
+                overview: film.overview,
+                id: film.id,
+            };
+        };
+    };
+    
+    if (findFilm) {
+        queue.splice(indexFilm, 1);
+        localstorage.save('queue', queue);
+        queueBtn.classList.remove('button-list--active');
+        queueBtn.textContent = 'Add to queue';
+        location.reload();
     } else {
-      queueBtn.textContent = 'Add to queue';
-    }
-  } else {
-    queue.push(results);
-    localstorage.save('queue', queue);
-    queueBtn.classList.add('button-list--active');
-    if (localStorage.getItem('language') === 'ua') {
-      queueBtn.textContent = 'Видалити з черги';
+        queue.push(results);
+        localstorage.save('queue', queue);
+        queueBtn.classList.add('button-list--active');
+        queueBtn.textContent = 'Remove from queue';
+    };
+};
+
+export function addToWatched(id) {
+    const filmsFromLocalStorage = localstorage.load('currentFilms');
+    const indexFilm = watched.findIndex(film => film.id === Number(id));
+    const findFilm = watched.find(film => film.id === Number(id));
+
+    let results = {};
+
+    for (const film of filmsFromLocalStorage.results) {
+        if (film.id === Number(id)) {
+            results = {
+                genre_ids: film.genre_ids,
+                release_date: film.release_date,
+                poster_path: film.poster_path,
+                backdrop_path: film.backdrop_path,
+                title: film.title,
+                popularity: film.popularity,
+                vote_average: film.vote_average,
+                vote_count: film.vote_count,
+                overview: film.overview,
+                id: film.id,
+            };
+        };
+    };
+    
+    if (findFilm) {
+        watched.splice(indexFilm, 1);
+        localstorage.save('watched', watched);
+        watchedBtn.classList.remove('button-list--active');
+        watchedBtn.textContent = 'Add to watched';
+        location.reload();
     } else {
-      queueBtn.textContent = 'Remove from queue';
-    }
-  }
-}
-
-function addToWatched(id) {
-  const filmsFromLocalStorage = localstorage.load('currentFilms');
-  const indexFilm = watched.findIndex(film => film.id === Number(id));
-  const findFilm = watched.find(film => film.id === Number(id));
-
-  let results = {};
-
-  for (const film of filmsFromLocalStorage.results) {
-    if (film.id === Number(id)) {
-      results = {
-        genre_ids: film.genre_ids,
-        release_date: film.release_date,
-        poster_path: film.poster_path,
-        backdrop_path: film.backdrop_path,
-        title: film.title,
-        popularity: film.popularity,
-        vote_average: film.vote_average,
-        vote_count: film.vote_count,
-        overview: film.overview,
-        id: film.id,
-      };
-    }
-  }
-
-  if (findFilm) {
-    watched.splice(indexFilm, 1);
-    localstorage.save('watched', watched);
-    watchedBtn.classList.remove('button-list--active');
-    if (localStorage.getItem('language') === 'ua') {
-      watchedBtn.textContent = 'Додати до переглянутого';
-    } else {
-      watchedBtn.textContent = 'Add to watched';
-    }
-  } else {
-    watched.push(results);
-    localstorage.save('watched', watched);
-    watchedBtn.classList.add('button-list--active');
-    if (localStorage.getItem('language') === 'ua') {
-      watchedBtn.textContent = 'Видалити з переглянутого';
-    } else {
-      watchedBtn.textContent = 'Remove from watched';
-    }
-  }
-}
+        watched.push(results);
+        localstorage.save('watched', watched);
+        watchedBtn.classList.add('button-list--active');
+        watchedBtn.textContent = 'Remove from watched';
+    };
+};
 
 function getBtnRefs(target) {
-  if (target.id === 'queueInModal') {
-    queueBtn = target;
-    return;
-  }
-  if (target.id === 'watchedInModal') {
-    watchedBtn = target;
-    return;
-  }
-}
+    if (target.id === 'queueInModal') {
+        queueBtn = target;
+        return;
+    };
+    if (target.id === 'watchedInModal') {
+        watchedBtn = target;
+        return;
+    };
+};
 
 // async function appendToFilmContainer(parsedFilms) {
 //     try {
