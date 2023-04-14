fc41b7d0ca042498de369a36d54f80cb57caf7f5
Olga Tolstykhina <ovt0709@gmail.com>
2023-04-14 12:09:15 +0300

    modal-bug-fix
     if (
            document.querySelector('.nav__link--current').textContent === 'Home' ||
            'Головна'
          ) - НЕ ВІРНО

--- src/js/service/catcherror.js
+++ src/js/service/catcherror.js
@@ -2,6 +2,6 @@ import notifier from './notifier'
 
 export default function catchError(error, message) {
     notifier.error(message);
-    console.error('Error message: ', error.message);
+    // console.error('Error message: ', error.message);
     console.error('Error stack: ', error.stack);
     };
