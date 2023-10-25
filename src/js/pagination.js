a9e93674f440c516b54efccf8da496454c1ba8e7
Nadiia Maltseva <amanavi2021@gmail.com>
2023-10-25 14:33:44 +0300

    Update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -6,143 +6,164 @@ import localstorage from './service/localstorage';
 const NUMBER_PAGINATION = 5;
 const cardContainerEl = document.querySelector('.gallery');
 const paginationEl = document.querySelector('.pagination__box');
-const paginationContainerEl = document.querySelector('.pagination__container')
+const paginationContainerEl = document.querySelector('.pagination__container');
 const formRef = document.querySelector('#search-form');
 
 function getFilmsFromStoradge() {
-    const savedFilms = localStorage.getItem('currentFilms') || {};
-    try {
-        const parsedSavedFilms = JSON.parse(savedFilms);
-        const pageNumber = parsedSavedFilms.total_pages;
+  const savedFilms = localStorage.getItem('currentFilms') || {};
+  try {
+    const parsedSavedFilms = JSON.parse(savedFilms);
+    const pageNumber = parsedSavedFilms.total_pages;
 
-        return pageNumber;
-    } catch {
-        console.log('nothing');
-    }
+    return pageNumber;
+  } catch {
+    console.log('nothing');
+  }
 }
 
 export async function paginationFeach() {
+  const totalPages = Math.min(getFilmsFromStoradge(), 500);
 
-    const totalPages = getFilmsFromStoradge();
-    
-    await onLoad(); 
-    
-    if (totalPages <= NUMBER_PAGINATION + 2 && totalPages >= 1) {
+  await onLoad();
 
-        paginationFunctions.displayPaginationSmall(totalPages, paginationEl);
-        document.querySelector('.js-page-1').classList.add('pagination__item--select');
+  if (totalPages <= NUMBER_PAGINATION + 2 && totalPages >= 1) {
+    paginationFunctions.displayPaginationSmall(totalPages, paginationEl);
+    document
+      .querySelector('.js-page-1')
+      .classList.add('pagination__item--select');
 
-         if (totalPages === 1) {
-                paginationFunctions.clearPagination(paginationEl);
-         }
-        
-        paginationEl.addEventListener('click', onPaginationBtnClick);
-        formRef.addEventListener('submit', onFormClickSmall);
-
-        function onFormClickSmall() {
-            paginationEl.removeEventListener('click', onPaginationBtnClick);
-            paginationContainerEl.removeEventListener('click', onPaginationClick);
-        }
-        return;
+    if (totalPages === 1) {
+      paginationFunctions.clearPagination(paginationEl);
     }
 
-    paginationEl.innerHTML = '';
-    
-    paginationFunctions.displayPaginationBig(totalPages, paginationContainerEl, paginationEl);
-    document.querySelector('.js-page-1').classList.add('pagination__item--select');
+    paginationEl.addEventListener('click', onPaginationBtnClick);
+    formRef.addEventListener('submit', onFormClickSmall);
 
-    const btnLeft = document.querySelector('.pagination__btnLeft');
-    const btnRight = document.querySelector('.pagination__btnRight');
+    function onFormClickSmall() {
+      paginationEl.removeEventListener('click', onPaginationBtnClick);
+      paginationContainerEl.removeEventListener('click', onPaginationClick);
+    }
+    return;
+  }
 
-    btnLeft.disabled = true;
+  paginationEl.innerHTML = '';
 
-    paginationContainerEl.addEventListener('click', onPaginationClick);
+  paginationFunctions.displayPaginationBig(
+    totalPages,
+    paginationContainerEl,
+    paginationEl
+  );
+  document
+    .querySelector('.js-page-1')
+    .classList.add('pagination__item--select');
 
-    function onPaginationClick (e) {
-        e.preventDefault();
+  const btnLeft = document.querySelector('.pagination__btnLeft');
+  const btnRight = document.querySelector('.pagination__btnRight');
 
-        if (!e.target.classList.contains('pagination__item') && !e.target.classList.contains('pagination__btnLeft') && !e.target.classList.contains('pagination__btnRight') ) {
-            return;
-        };
-        
-        if (e.target.classList.contains('pagination__item')) {
-             const currentPage = Number(e.target.textContent);        
-        
-            onBigPaginationBtnClickrRenderFilms(currentPage);
-            onBigPaginationBtnClickrRenderPagination(totalPages, currentPage); 
+  btnLeft.disabled = true;
 
-            paginationFunctions.activityArrows(currentPage, totalPages);
-        }
+  paginationContainerEl.addEventListener('click', onPaginationClick);
 
-        if (e.target.classList.contains('pagination__btnLeft')) {
-               
-            const activePage = document.querySelector('.pagination__item--select');
-            const activePageNumber = Number(activePage.textContent);
-            const previousPage = activePageNumber - 1;
+  function onPaginationClick(e) {
+    e.preventDefault();
 
-            onBigPaginationBtnClickrRenderFilms(previousPage);
-            onBigPaginationBtnClickrRenderPagination(totalPages, previousPage); 
-            
-            paginationFunctions.activityOfLeftArrow(1);
-            }
-
-        if (e.target.classList.contains('pagination__btnRight')) {
-             
-            const activePage = document.querySelector('.pagination__item--select');
-            const activePageNumber = Number(activePage.textContent); 
-            const nextPage = activePageNumber + 1;
-
-            onBigPaginationBtnClickrRenderFilms(nextPage);
-            onBigPaginationBtnClickrRenderPagination(totalPages, nextPage);    
-            
-            paginationFunctions.activityOfRightArrow(totalPages);
-        }
+    if (
+      !e.target.classList.contains('pagination__item') &&
+      !e.target.classList.contains('pagination__btnLeft') &&
+      !e.target.classList.contains('pagination__btnRight')
+    ) {
+      return;
     }
 
-    formRef.addEventListener('submit', onFormClick);
+    if (e.target.classList.contains('pagination__item')) {
+      const currentPage = Number(e.target.textContent);
 
-    function onFormClick() {
-        paginationContainerEl.removeEventListener('click', onPaginationClick);
-        paginationEl.removeEventListener('click', onPaginationBtnClick);
+      onBigPaginationBtnClickrRenderFilms(currentPage);
+      onBigPaginationBtnClickrRenderPagination(totalPages, currentPage);
+
+      paginationFunctions.activityArrows(currentPage, totalPages);
     }
+
+    if (e.target.classList.contains('pagination__btnLeft')) {
+      const activePage = document.querySelector('.pagination__item--select');
+      const activePageNumber = Number(activePage.textContent);
+      const previousPage = activePageNumber - 1;
+
+      onBigPaginationBtnClickrRenderFilms(previousPage);
+      onBigPaginationBtnClickrRenderPagination(totalPages, previousPage);
+
+      paginationFunctions.activityOfLeftArrow(1);
+    }
+
+    if (e.target.classList.contains('pagination__btnRight')) {
+      const activePage = document.querySelector('.pagination__item--select');
+      const activePageNumber = Number(activePage.textContent);
+      const nextPage = activePageNumber + 1;
+
+      onBigPaginationBtnClickrRenderFilms(nextPage);
+      onBigPaginationBtnClickrRenderPagination(totalPages, nextPage);
+
+      paginationFunctions.activityOfRightArrow(totalPages);
+    }
+  }
+
+  formRef.addEventListener('submit', onFormClick);
+
+  function onFormClick() {
+    paginationContainerEl.removeEventListener('click', onPaginationClick);
+    paginationEl.removeEventListener('click', onPaginationBtnClick);
+  }
 }
 
 function onBigPaginationBtnClickrRenderPagination(totalPages, currentPage) {
-    paginationFunctions.clearPagination(paginationEl);
+  paginationFunctions.clearPagination(paginationEl);
 
-    const n = NUMBER_PAGINATION - (Math.ceil(NUMBER_PAGINATION / 3) - 2);
-    const m = totalPages - (NUMBER_PAGINATION - 1);
+  const n = NUMBER_PAGINATION - (Math.ceil(NUMBER_PAGINATION / 3) - 2);
+  const m = totalPages - (NUMBER_PAGINATION - 1);
 
+  if (currentPage < n) {
+    paginationFunctions.displayPaginationBig(
+      totalPages,
+      paginationContainerEl,
+      paginationEl
+    );
+  } else if (currentPage >= n && currentPage <= m) {
+    paginationFunctions.displayPaginationBigMiddle(
+      totalPages,
+      currentPage,
+      paginationContainerEl,
+      paginationEl
+    );
+  } else {
+    paginationFunctions.displayPaginationBigFinish(
+      totalPages,
+      paginationContainerEl,
+      paginationEl
+    );
+  }
 
-    if (currentPage < n) {
-        paginationFunctions.displayPaginationBig(totalPages, paginationContainerEl, paginationEl);
-    } else if (currentPage >= n && currentPage <= m) {
-        paginationFunctions.displayPaginationBigMiddle(totalPages, currentPage, paginationContainerEl, paginationEl);
-    } else {
-        paginationFunctions.displayPaginationBigFinish(totalPages, paginationContainerEl, paginationEl);
-    }
-
-    document.querySelector(`.js-page-${currentPage}`)?.classList.add('pagination__item--select');
+  document
+    .querySelector(`.js-page-${currentPage}`)
+    ?.classList.add('pagination__item--select');
 }
 
 async function onBigPaginationBtnClickrRenderFilms(currentPage) {
-    cardContainerEl.innerHTML = '';
-    apiService.pageNumber = currentPage;
-    await onLoad();
+  cardContainerEl.innerHTML = '';
+  apiService.pageNumber = currentPage;
+
+  await onLoad();
 }
 
-
 async function onPaginationBtnClick(e) {
-    if (!e.target.classList.contains('pagination__item')) {
-        return;
-    }
-    cardContainerEl.innerHTML = '';
-    apiService.pageNumber = Number(e.target.textContent);
+  if (!e.target.classList.contains('pagination__item')) {
+    return;
+  }
+  cardContainerEl.innerHTML = '';
+  apiService.pageNumber = Number(e.target.textContent);
 
-    await onLoad();
+  await onLoad();
 
-    document.querySelector(`.js-page-${currentPage}`).classList.add('pagination__item--select');
-}   
-
-
-
+  document
+    .querySelector(`.js-page-${currentPage}`)
+    .classList.add('pagination__item--select');
+}
